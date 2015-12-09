50d3a89abcc10ded48e25fd5f1778372b0c7a868
milleniumbug <wojas1222@gmail.com>
2015-12-10 00:11:17 +0100

    improved test of reading

--- proto/open.cpp
+++ proto/open.cpp
@@ -8,15 +8,24 @@
 #include <string.h>
 #include <inttypes.h>
 
+void test_read(const char* path, size_t size)
+{
+    int fd = open(path, O_RDWR);
+    printf("open: %s\n", strerror(errno));
+    char buf[4096] = { 0 };
+    {
+        int r = read(fd, buf, size < sizeof buf ? size : sizeof buf);
+        printf("read: %d %s\n", r, strerror(errno));
+    }
+    close(fd);
+}
+
 int main()
 {
     const char* path = "/home/milleniumbug/dokumenty/PROJEKTY/InDevelopment/OS_Projekt_WTFS/asdf/asdf/koles";
-    int fd = open(path, O_RDWR|O_CREAT);
-    printf("open: %s\n", strerror(errno));
-    char buf[4096] = { 0 };
-    read(fd, buf, sizeof buf);
-    printf("read: %s\n", strerror(errno));
-    off_t newoff = lseek(fd, 0, SEEK_END);
-    printf("lseek: %s\n", strerror(errno));
-    close(fd);
+    test_read(path, 0);
+    test_read(path, 3);
+    test_read(path, 5);
+    test_read(path, 10);
+    test_read(path, 4000);
 }
