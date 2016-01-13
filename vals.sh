9204078df3f93065d3f5886c8fbc1594e85b4767
milleniumbug <wojas1222@gmail.com>
2016-01-13 16:49:37 +0100

    changed default run commands

--- vals.sh
+++ vals.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-valgrind --vgdb=yes --vgdb-error=0 ./OS_Projekt_WTFS -o uid=1000,gid=1000,allow_other,default_permissions -d -f asdf
+valgrind --vgdb=yes --vgdb-error=0 ./OS_Projekt_WTFS fs -o uid=1000,gid=1000 -d -f asdf
